717c7e00f060002b173a41d83b45e1eabe53c042
Mugabo Andre <andremugabo@yahoo.fr>
2025-06-21 22:26:54 +0200

    home page done

---
+++ src/main/java/com/ma/codinglab/shopease/controller/ShopEaseRouterController.java
@@ -0,0 +1,27 @@
+package com.ma.codinglab.shopease.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShopEaseRouterController {
+    @GetMapping("/")
+    public String homepage(){
+        return "index";
+    }
+    @GetMapping("/categories")
+    public String categories(){
+        return "categories/index";
+    }
+    @GetMapping("/products")
+    public String products(){
+        return "products/index";
+    }
+
+
+    @GetMapping("/sales")
+    public String sales(){
+        return "sales/index";
+    }
+}
